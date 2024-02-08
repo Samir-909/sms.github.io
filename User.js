c9df2e99bfbc10fefddf447e1c64eee476b2f100
Samir-909 <159404391+Samir-909@users.noreply.github.com>
2024-02-08 09:51:59 +0530

    Add files via upload

---
+++ User.js
@@ -0,0 +1,19 @@
+var a = "sms09"
+var b = "sms9"
+               
+pass = prompt("Enter your name that I verify its you");
+
+                if(pass==a)
+                {
+                    window.location="class-09.html";
+                }
+                else
+                {
+                    window.location="error.html"
+                }
+                if (pass==b) 
+                {
+                    window.location="teachers-9.html";     
+                }
+              
+                
